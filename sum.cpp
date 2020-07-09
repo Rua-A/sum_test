e18094e3d0ac39e7bd61d763640df8970b0180fc
Rua-A <rootxbot@gmail.com>
2020-07-09 11:45:18 +0900

    correction of code typos

--- sum.cpp
+++ sum.cpp
@@ -1,10 +1,11 @@
-#include "sum.h"
+# include "sum.h"
 
-int sum(int n){
-	int sum = 0;
-	
-	for(int i=1;i<= n; i++){
-		sum +=1;
-	}
-	return sum;
+int sum(int n) {
+    int result = 0;
+
+    for(int i=1 ; i<=n ; i++) {
+        result += i;
+    }
+
+    return result;
 }
